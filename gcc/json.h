186dae32039c2f51c811cd94e4c29836b170f7b9
Yangyu Chen <cyy@cyyself.name>
2025-06-30 02:57:13 +0800

    json: add iterate method to object class
    Currently, the object class in json.h does not provide a way to iterate
    over its keys. This commit adds an `iterate` method to the object class,
    allowing users to iterate over the keys of a JSON object.
    
    Signed-off-by: Yangyu Chen <cyy@cyyself.name>
    
    gcc/ChangeLog:
    
    	* json.h (class object): Add iterate method to object class.

--- gcc/json.h
+++ gcc/json.h
@@ -183,6 +183,11 @@ class object : public value
   /* Set to literal true/false.  */
   void set_bool (const char *key, bool v);
 
+  bool iterate (unsigned i, const char **key) const
+  {
+    return m_keys.iterate (i, key);
+  }
+
   static int compare (const json::object &obj_a, const json::object &obj_b);
 
   size_t get_num_keys () const { return m_keys.length (); }
